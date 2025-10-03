08f794c8385fbd1ab09b98695cdce9ceb20e9425
Adam Worede <adamg2609@gmail.com>
2025-10-03 23:52:16 +0100

    additional data points

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -1,9 +1,10 @@
 import requests
 import json
-countries_endpoint = "https://api.worldbank.org/v2/country/?format=json"
-# ADD A DATE ARGUMENT TO GET A RANGE OF VALUES
-gdp_endpoint = "https://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?date=1990:2024&format=json"
 
+# ENDPOINTS
+countries_endpoint = "https://api.worldbank.org/v2/country/?format=json"
+gdp_endpoint = "https://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?date=1990:2024&format=json"
+population_endpoint = "https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?date=1990:2024&format=json"
 base_api = "https://search.worldbank.org/api/v3/wds?format=json/country"
 
 worldbank_indicators = [
@@ -11,7 +12,7 @@ worldbank_indicators = [
 ]
 
 # world bank countries list
-def list_countries():
+def get_countries_list():
     countries_response = requests.get(countries_endpoint)
     json_response = json.loads(countries_response.text)
     return json_response[1]
@@ -24,4 +25,10 @@ def get_gdp_figures():
     return json_response[1]
 
 
-print(get_gdp_figures()) 
+def get_population_figures():
+    pop_response = requests.get(population_endpoint)
+    json_response = json.loads(pop_response.text)
+    return json_response[1]
+
+
+print(get_population_figures()) 
