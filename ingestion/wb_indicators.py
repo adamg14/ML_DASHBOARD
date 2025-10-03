91dc6b67a9cbdcacda0f3e6d1cd3adbd0866668d
Adam Worede <adamg2609@gmail.com>
2025-10-04 00:33:05 +0100

    world bank indicators added

---
+++ ingestion/wb_indicators.py
@@ -0,0 +1,32 @@
+WORLD_BANK_INDICATORS = [
+    # GDP
+    "NY.GDP.MKTP.CD",
+    # GDP PER CAPITA
+    "NY.GDP.PCAP.CD"
+    # inflation rate
+    "FP.CPI.TOTL.ZG",
+    # total population
+    "SP.POP.TOTL",
+    # life expectancy
+    "SP.DYN.LE00.IN",
+    # school enrollment
+    "SE.PRM.NENR",
+    # adult literacy rate
+    "SE.ADT.LITR.ZS",
+    # % OF GDP ON education
+    "SE.XPD.TOTL.GD.ZS",
+    # child mortality rate
+    "SH.DYN.MORT",
+    # CO2 emissions
+    "EN.ATM.CO2E.PC",
+    # renewable energy consumption % of total
+   " EG.FEC.RNEW.ZS",
+    # access to electricity
+    "EG.ELC.ACCS.ZS",
+    # exports
+    "NE.EXP.GNFS.ZS",
+    # imports
+    "NE.IMP.GNFS.ZS",
+    # aid received
+    "DT.ODA.ODAT.CD"
+]
